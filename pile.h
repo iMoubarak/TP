b9c9a86a1a3bd9cbff19193ce7e18f373658dfd6
DERKARIOM_2 <abdoulkaderbachir34@gmail.com>
2022-07-18 21:14:45 +0200

    ajout de la fonction empiler(),depiler() et affichier_pile()

--- pile.h
+++ pile.h
@@ -4,3 +4,6 @@ typedef struct pile
     struct pile *suivant;
 }*pile;
 pile pile_vide();
+pile empiler(pile p , int e);
+pile depiler(pile p);
+void affichier_pile(pile p);
--- source.c
+++ source.c
@@ -5,3 +5,33 @@ pile pile_vide()
 {
     return NULL;
 }
+pile empiler(pile p , int e)
+{
+    pile cellule;
+    cellule = (pile)malloc(sizeof(cellule));
+    cellule->n = e;
+    cellule->suivant = p;
+    return cellule;
+}
+pile depiler(pile p)
+{
+    pile supp;
+    if (p == NULL)
+        printf("LA PILE EST VIDE\n");
+    else
+    {
+        supp = p;
+        p = p->suivant;
+    }
+    return p;
+}
+void affichier_pile(pile p)
+{
+    pile tete;
+    tete = p;
+    while(p != NULL)
+    {
+        printf("%d\n" , p->n);
+        p = p->suivant;
+    }
+}
