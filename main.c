ba778bb607a28d2c55ba35b554a92b20e965ec57
DERKARIOM_2 <abdoulkaderbachir34@gmail.com>
2022-07-18 22:02:28 +0200

    ajout de la fonction main()

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pile.h"
+int main()
+{
+    pile p;
+    p = pile_vide();
+    p = empiler(p,1);
+    p = empiler(p,2);
+    p = empiler(p,3);
+    p = depiler(p);
+    affichier_pile(p);
+    return 0;
+}
